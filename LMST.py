abf0d84e456098fd15fe555103547203862cfed6
Mika Myrseth <mika@myrseth.org>
2021-10-24 13:54:45 +0200

    feat(LMST): get all rows with specific customer

--- LMST.py
+++ LMST.py
@@ -1,4 +1,6 @@
+from typing import List
 import pandas as pd
+from pandas.core.frame import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px  # to plot the time series plot
@@ -7,4 +9,16 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 import tensorflow as tf
 
 
-data = pd.read_csv("Output.csv", decimal=",")
+def get_customer_ids(data: DataFrame) -> List[int]:
+    return data['CUSTOMER_ID'].unique()
+
+
+def get_all_rows_with_customer(data: DataFrame, customer_id: int) -> List:
+    return data[data["CUSTOMER_ID"] == customer_id]
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("Output.csv", decimal=",")
+    customer_ids = get_customer_ids(data)
+    for customer_id in customer_ids:
+        customer_data = get_all_rows_with_customer(data, customer_id)
