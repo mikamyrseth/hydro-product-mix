93a3dbd878e3cce5af17247032096a7f680bfd2d
le0-0 <leonard.schoyen@gmail.com>
2021-11-03 20:48:57 +0100

    Partial implementation of preprocessing

--- LSTM.py
+++ LSTM.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
 from tensorflow import keras
@@ -45,10 +46,10 @@ def clean_data() -> None:
 
 def create_model(data: DataFrame):
     model = keras.Sequential([
-        layers.Input(batch_shape=(None, None, 3933)),
+        layers.Input(batch_shape=(None, None, 3934)),
         layers.LSTM(256, return_sequences=True),
         layers.LSTM(256),
-        layers.Dense(3933),
+        layers.Dense(3934),
     ])
 
     # Fit
@@ -74,7 +75,33 @@ def preprocess(data: DataFrame) -> DataFrame:
     :param data: DataFrame with data to preprocess.
     :return: DataFrame with preprocessed data.
     """
-    pass
+
+    sequence_map: dict[tuple, DataFrame] = dict()
+
+    def ensure_sequence_key(product_mix: DataFrame):
+        # print(product_mix.items()[0][Column.AREA])
+        key = (product_mix[Column.AREA].iloc[0], product_mix[Column.PRODUCT_TYPE_ID].iloc[0],
+               product_mix[Column.PRODUCT_CATEGORY].iloc[0],
+               product_mix[Column.CUSTOMER_ID].iloc[0])
+        print(key)
+        if key not in sequence_map:
+            d = DataFrame(np.zeros(shape=(104, 3934)), dtype=pd.SparseDtype("float", 0))
+            d[0] = pd.arrays.SparseArray(np.ones(104, ), dtype=pd.SparseDtype("float", 1))
+            sequence_map[key] = d
+            print(d.memory_usage())
+            print(d.dtypes)
+            print(d.sparse.density)
+
+    def a(product_mix: DataFrame):
+        ensure_sequence_key(product_mix)
+
+        for index, row in product_mix.iterrows():
+            print(row[Column.PRODUCT_ID])
+            print(row[Column.PRODUCT_FRACTION])
+        exit(0)
+
+    data.sort_values(by=PROD_MIX_KEY, inplace=True)
+    data.groupby(PROD_MIX_KEY).apply(a)
 
 
 if __name__ == "__main__":
