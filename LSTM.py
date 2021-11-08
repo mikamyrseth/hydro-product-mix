eb4d0690c6ecbbeb0928c4f7cd881f186adfef0c
le0-0 <leonard.schoyen@gmail.com>
2021-11-08 14:30:46 +0100

    Model now runs.

--- LSTM.py
+++ LSTM.py
@@ -1,11 +1,10 @@
-from typing import Optional
-
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
-from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from pandas import DataFrame
+from scipy.sparse import dok_matrix
+from sklearn.preprocessing import LabelEncoder
 
 from data.raw import Column
 
@@ -43,19 +42,35 @@ def clean_data() -> None:
         for index, _ in ting.iterrows():
             data.loc[index, Column.PRODUCT_FRACTION] /= 2
 
+    data = data.groupby(
+        [Column.AREA, Column.PRODUCT_TYPE_ID, Column.PRODUCT_CATEGORY, Column.CUSTOMER_ID,
+         Column.YEAR, Column.MONTH, Column.PRODUCT_ID]
+    )[Column.PRODUCT_FRACTION].agg("sum")
+
     # Save cleaned data
     data.to_csv("./data/cleaned.csv", sep=";")
 
 
-def create_model(data: DataFrame):
+def create_model():
     model = keras.Sequential([
-        layers.Input(batch_shape=(None, None, 3934)),
-        layers.LSTM(256, return_sequences=True),
-        layers.LSTM(256),
-        layers.Dense(3934),
+        keras.layers.Input(batch_shape=(None, None, 3934)),
+        keras.layers.LSTM(1024, return_sequences=True),
+        keras.layers.LSTM(1024),
+        keras.layers.Dense(3934),
     ])
 
-    # Fit
+    model.compile(
+        optimizer="adam",
+        loss="mse",
+        metrics=["accuracy"],
+    )
+
+    return model
+
+
+def fit_model(data, model):
+
+    model.fit(data.batch(690), epochs=5)
 
     return model
 
@@ -64,19 +79,25 @@ def main():
     # clean_data()
 
     data = pd.read_csv("./data/cleaned.csv", sep=";")
-    data = preprocess(data)
-    model = create_model(data)
+    dataset = preprocess(data)
+    model = create_model()
+    model = fit_model(dataset, model)
+    model.save('models/model_2')
 
-    print(model.summary())
+    # model = tf.keras.models.load_model('models/model_1')
+    # for x, y in dataset.skip(800).take(1):
+    #     np.savetxt("x", x.numpy(), "%.6f")
+    #     np.savetxt("y", y.numpy(), "%.6f")
+    #     np.savetxt("p", tf.reshape(model.predict(tf.reshape(x, (1, 26, 3934))), (3934,)), "%.6f")
 
 
-def preprocess(data: DataFrame) -> DataFrame:
+def preprocess(data: DataFrame):
     """ Pre-process data.
 
     Data should already be cleaned.
 
     :param data: DataFrame with data to preprocess.
-    :return: DataFrame with preprocessed data.
+    :return: Sparse matrices containing Xs and Ys.
     """
 
     # Encode product IDs
@@ -86,7 +107,7 @@ def preprocess(data: DataFrame) -> DataFrame:
 
     # Store product mixes in customer history blocks
 
-    customer_history_map: dict[tuple, DataFrame] = dict()
+    customer_history_map: dict[tuple, dok_matrix] = dict()
 
     def generate_customer_key(product_mix: DataFrame) -> tuple:
         return (product_mix[Column.AREA].iloc[0], product_mix[Column.PRODUCT_TYPE_ID].iloc[0],
@@ -94,19 +115,14 @@ def preprocess(data: DataFrame) -> DataFrame:
                 product_mix[Column.CUSTOMER_ID].iloc[0])
 
     def get_product_mix_index(product_mix: DataFrame) -> int:
-        # (1, 2015) is 26 element (index 25)
-        # (M-1) + 12(Y-2015) + 25
-        # M + 12Y - 1 - 12*2015 + 25
-        return product_mix[Column.MONTH].iloc[0] + 12 * product_mix[Column.YEAR].iloc[0] - 24156
-
-    last_key: Optional[tuple] = None
+        # (1, 2015) is 27 element (index 26)
+        # (M-1) + 12(Y-2015) + 26
+        # M + 12Y - 1 - 12*2015 + 26
+        return product_mix[Column.MONTH].iloc[0] + 12 * product_mix[Column.YEAR].iloc[0] - 24155
 
     def prepare_customer_history(product_mix: DataFrame) -> tuple:
-        """ Ensure customer, with area, product type and category has a DataFrame in the history to
-        store product mixes in.
-
-        It is assumed that all product mixes with the same consumer will appear in a row. The
-        moment a new key appears, the old customer history will be converted to an immutable format.
+        """ Ensure customer, with area, product type and category has a sparse matrix in the
+        history to store product mixes in.
 
         :param product_mix: The product mix which customer information should be extracted from.
         :return: Key where customer product mixes are stored in history.
@@ -114,29 +130,41 @@ def preprocess(data: DataFrame) -> DataFrame:
         key = generate_customer_key(product_mix)
         if key not in customer_history_map:
             # Create new DataFrame
-            d = DataFrame(np.zeros(shape=(104, 3934)), dtype="float32")
-            d[0] = 1
+            d = dok_matrix((105, 3934), dtype="float32")
+            d[:, 0] = 1
             customer_history_map[key] = d
-
-            nonlocal last_key
-            # Clean up previous DataFrame
-            if last_key is not None:
-                customer_history_map[last_key] = customer_history_map[last_key].astype()
-            last_key = key
         return key
 
     def save_product_mix(product_mix: DataFrame) -> None:
         key = prepare_customer_history(product_mix)
         product_mix_index = get_product_mix_index(product_mix)
+        customer_history_map[key][product_mix_index, 0] = 0
         for index, row in product_mix.iterrows():
             product_id = row[Column.PRODUCT_ID]
-            sparse_data_type = customer_history_map[key].dtypes[product_id]
-            customer_history_map[key][product_id] = customer_history_map[key][product_id].sparse.to_dense()
-            customer_history_map[key].loc[product_mix_index, product_id] = row[Column.PRODUCT_FRACTION]
-            customer_history_map[key][product_id] = customer_history_map[key][product_id].astype(sparse_data_type)
+            customer_history_map[key][product_mix_index, product_id] = row[Column.PRODUCT_FRACTION]
+
+    print("Customer history construction started")
 
     data.sort_values(by=PROD_MIX_KEY, inplace=True)
     data.groupby(PROD_MIX_KEY).apply(save_product_mix)
+    print("\tCustomer records completely traversed")
+
+    def data_generator():
+        histories = customer_history_map.values()
+        for history in histories:
+            dense = history.toarray()
+            for i in range(26, 105):
+                yield dense[i - 26:i, :], dense[i, :]
+
+    dataset = tf.data.Dataset.from_generator(
+        data_generator,
+        output_signature=(
+            tf.TensorSpec(shape=(26, 3934), dtype=tf.float32),
+            tf.TensorSpec(shape=(3934,), dtype=tf.float32),
+        )
+    )
+
+    return dataset
 
 
 if __name__ == "__main__":
